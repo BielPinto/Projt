eeeaa014a363e4dc1a964ee11c4390c42429a369
BielPinto <gabrielp.roch@hotmail.com>
2018-10-25 00:58:47 -0300

    atualizando alterações

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
- 
+ /*
+git init   = iniciar o git 
+git add index.php  = iniciando o arquivo 
+git git add --all  adcionando todas as alterações na margen
+git commit -m "criei meu repositorio " = levantando e comentando a alteração 
+git remute add origem https://github.com/BielPinto/Proj_Test.git 
+git config --global user.name "bielpinto"
+git config --global user.email "gabrielp.roch@hotmail.com"
+git push origem master
+git remote add origem https://github.com/BielPinto/Proj_Test.git
+git push origin master  = jogar o codigo para o github
+
+ */
  
  echo "heloo word  ";
  echo " oi ";
