98b7dd81abfb66233f960c5593bd66e5c5d420a0
BielPinto <38528946+BielPinto@users.noreply.github.com>
2018-10-26 20:22:20 -0300

    atualizando

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
  /*
-git init   = iniciar o git 
-git add index.php  = iniciando o arquivo 
-git git add --all  adcionando todas as alterações na margen
-git commit -m "criei meu repositorio " = levantando e comentando a alteração 
-git remote add origem https://github.com/BielPinto/Proj_Test.git 
-git config --global user.name "bielpinto"
+ git config --global user.name "bielpinto"
 git config --global user.email "gabrielp.roch@hotmail.com"
-git push origem master
-git remote add origem https://github.com/BielPinto/Proj_Test.git
-git push origin master  = jogar o codigo para o github
-testando pull
+======================
+ cd C:\xampp\htdocs\php 
+git init   = iniciar o git 
+git add * ou   git add --all  = iniciando o arquivo   adcionando todas as alterações na margen
+git commit -m "criei meu repositorio " = levantando e comentando a alteração 
+git remote add origim https://github.com/BielPinto/Proj_Test.git 
+
+git push -u origin master  = jogar o codigo para o github
+
  */
  
  echo "heloo word  ";
